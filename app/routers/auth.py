1f54a8ebc549034d419d2ac99f860687384458a0
Vinod <vmurugan@ext.teachs.ai>
2026-02-08 21:08:29 +0530

    Validate JWT token (for Users/Posts/login path)

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -6,16 +6,16 @@ from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router=APIRouter()
 
-@router.post("/login")
+@router.post("/login", response_model=schemas.Token)
 def userlogin(user_credentials:OAuth2PasswordRequestForm=Depends(), db:Session=Depends(get_db)):
     db_user=db.query(models.User).filter(models.User.email==user_credentials.username).first()
 
     if not db_user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credentails")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid credentails")
     
     if not utils.verify(user_credentials.password, db_user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credentails")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid credentails")
     
     access_token=oauth2.create_access_token(data={"user_id":db_user.id})
 
-    return {"access_token":access_token,"token_type":"bearer"}
+    return {"access_token":access_token["token"],"token_type":"bearer"}
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,6 @@
 from typing import List
 from fastapi import status, Response, HTTPException, Depends, APIRouter
-from ..import schemas, models
+from ..import schemas, models, oauth2
 from sqlalchemy.orm import Session
 from ..database import get_db
 
@@ -10,17 +10,17 @@ router=APIRouter(
 )
 
 @router.get("/",response_model=List[schemas.Post])
-def get_posts(db:Session=Depends(get_db)):
+def get_posts(db:Session=Depends(get_db), current_user:int=Depends(oauth2.get_current_user)):
     #cursor.execute("""SELECT * FROM posts""")
     #posts=cursor.fetchall()
     posts=db.query(models.Post).all()
     return posts
 
 
-@router.post("/",  status_code=201, response_model=schemas.Post)
-def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
+@router.post("/",  status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user:int=Depends(oauth2.get_current_user)):
+    print(current_user.email)
     new_post = models.Post(**post.dict())
-
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -28,7 +28,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/{id}",response_model=schemas.Post)
-def get_post(id: int, db:Session=Depends(get_db)):
+def get_post(id: int, db:Session=Depends(get_db), current_user:int =Depends(oauth2.get_current_user)):
     #cursor.execute("""select * from posts where id=%s""", (str(id)))
     #test_post=cursor.fetchone()
     test_post=db.query(models.Post).filter(models.Post.id==id).first()
@@ -39,35 +39,38 @@ def get_post(id: int, db:Session=Depends(get_db)):
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db:Session= Depends(get_db)):
+def delete_post(id: int, db:Session= Depends(get_db), current_user:int=Depends(oauth2.get_current_user)):
     #cursor.execute("""delete from posts where id = %s returning *""",str(id))
     #deleted_post=cursor.fetchone()
     #conn.commit()
-    deleted_post=db.query(models.Post).filter(models.Post.id==id).first()
+    post_query =db.query(models.Post).filter(models.Post.id==id)
+    deleted_post=post_query.first()
 
     if deleted_post==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'post with id :{id} not found')
-    db.delete(deleted_post, synchronize_session=False)
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}",response_model=schemas.Post)
-def update_post(id:int, post:schemas.PostCreate, db:Session=Depends(get_db)):
+def update_post(id:int, updated_post:schemas.PostCreate, db:Session=Depends(get_db), 
+                current_use: int =Depends(oauth2.get_current_user)):
         #cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s where id = %s RETURNING *"""),
-        #( post.title, post.content, post.published, str(id))
+        #( post.title, post.content, post.published, str(id))   
         #updated_post=cursor.fetchone()
         #conn.commit()
         ''' index=find_index_post(id) '''
         post_query=db.query(models.Post).filter(models.Post.id==id)
-        updated_post=post_query.first()
-        if updated_post==None:
+        post=post_query.first()
+        if post==None:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail=f'post with id : {id} not found')
-        post_query.update(post.dict(), 
+        post_query.update(updated_post.dict(), 
                     synchronize_session=False)
         db.commit()
+        db.refresh(post)
         '''post_dict=post.dict()
         post_dict['id']=id 
         my_posts[index]=post_dict '''
-        return updated_post
+        return post
